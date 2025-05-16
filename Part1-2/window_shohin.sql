2a7d54a7bee56570be1b66d9dc8e6faaffca4dc2
waisu <way.03.remi@gmail.com>
2025-05-17 00:26:00 +0900

    desktop PC

--- Part1-2/window_shohin.sql
+++ Part1-2/window_shohin.sql
@@ -23,3 +23,20 @@ WINDOW
             shohin_id ROWS BETWEEN 2 preceding
             AND CURRENT ROW
     );
+
+SELECT
+    shohin_id,
+    shohin_mei,
+    hanbai_tanka,
+    avg(hanbai_tanka) OVER W AS moving_avg,
+    sum(hanbai_tanka) OVER W AS moving_sum,
+    count(hanbai_tanka) OVER W AS moving_count,
+    max(hanbai_tanka) OVER W AS moving_max
+FROM
+    shohin
+WINDOW
+    W AS (
+        ORDER BY
+            shohin_id ROWS BETWEEN 2 preceding
+            AND CURRENT ROW
+    );
